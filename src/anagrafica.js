87e9ce45e26c8fcb7de394a7e69c60c0029bfa75
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-07 00:23:31 +0200

    Minchia funziona, hmmm

--- src/anagrafica.js
+++ src/anagrafica.js
@@ -1,52 +1,100 @@
+$(document).ready(function() {
 
-const tableModify = (id) => {
-    console.log("Modificato")
-}
-
-const tableRemove = (id) => {
-    console.log("Rimosso")
-}
-
-$(document).ready(function () {
-  $("#example").DataTable({
-    ajax: {
-      url: " http://localhost:3000/anagraficajson",
-      type: "GET",
-      dataSrc: "",
-      dataType: "json",
+  var columnDefs = [{
+    data: "id",
+    title: "Id",
+    type: "readonly"
+  },
+  {
+    data: "name",
+    title: "Name"
+  },
+ {
+    data: "position",
+    title: "Position"
+  },
+ {
+    data: "office",
+    title: "Office"
+  },
+ {
+    data: "extension",
+    title: "Extn."
+  },
+ {
+    data: "startDate",
+    title: "Start date"
+  },
+ {
+    data: "salary",
+    title: "Salary"
+  },
+ {
+    data: null,
+    title: "Actions",
+    name: "Actions",
+    render: function (data, type, row, meta) {
+      return '<a class="delbutton fa fa-minus-square btn btn-danger" href="#"></a>';
     },
-    columns: [
-      { data: "id" },
-      { data: "RagSoc" },
-      { data: "Address" },
-      { data: "City" },
-      { data: "Prov" },
-      { data: "CAP" },
-      { data: "PIVA" },
-      { data: "CF" },
-      { data: "Phone" },
-      { data: "Fax" },
-      { data: "Email" },
-    ],
+    disabled: true
+  } 
+  ];
 
-    columnDefs: [
-      {
-        targets: [0],
-        visible: false,
-        searchable: false,
-      },
-      {
-        render: function (data, type, full, row) {
-          return `<button class="btn btn-dark" id="${full.id}" onclick="tableModify(${full.id})">Modifica</button>`;
-        },
-        targets: 11,
-      },
-      {
-        render: function (data, type, full, row) {
-          return `<button class="btn btn-danger" id="${full.id}" onclick="tableRemove(${full.id})">Rimuovi</button>`;
-        },
-        targets: 12,
-      },
-    ],
+  var myTable;
+
+  myTable = $('#example').DataTable({
+    "sPaginationType": "full_numbers",
+    data: dataSet,
+    columns: columnDefs,
+	dom: 'Bfrtip',        // Needs button container
+    select: {
+        style: 'single',
+        toggleable: false
+    },
+    responsive: true,
+    altEditor: true,     // Enable altEditor
+    buttons: []          // no buttons, however this seems compulsory
+  });
+
+  // Edit
+  $(document).on('click', "[id^='example'] tbody ", 'tr', function () {
+    var tableID = $(this).closest('table').attr('id');    // id of the table
+    var that = $( '#'+tableID )[0].altEditor;
+    that._openEditModal();
+    $('#altEditor-edit-form-' + that.random_id)
+                .off('submit')
+                .on('submit', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    that._editRowData();
+                });
+  });
+
+  // Delete
+  $(document).on('click', "[id^='example'] .delbutton", 'tr', function (x) {
+    var tableID = $(this).closest('table').attr('id');    // id of the table
+    var that = $( '#'+tableID )[0].altEditor;
+    that._openDeleteModal();
+    $('#altEditor-delete-form-' + that.random_id)
+                .off('submit')
+                .on('submit', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    that._deleteRow();
+                });
+    x.stopPropagation(); //avoid open "Edit" dialog
+  });
+
+  // Add row
+  $('#addbutton').on('click', function () {
+    var that = $( '#example' )[0].altEditor;
+    that._openAddModal();
+    $('#altEditor-add-form-' + that.random_id)
+                .off('submit')
+                .on('submit', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    that._addRowData();
+                });
   });
 });
