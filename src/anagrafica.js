76fec34028979f29c42a02deb35c1d406af904f1
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-12 23:28:10 +0200

    Work from home

--- src/anagrafica.js
+++ src/anagrafica.js
@@ -1,100 +1,124 @@
-$(document).ready(function() {
-
-  var columnDefs = [{
-    data: "id",
-    title: "Id",
-    type: "readonly"
-  },
-  {
-    data: "name",
-    title: "Name"
-  },
- {
-    data: "position",
-    title: "Position"
-  },
- {
-    data: "office",
-    title: "Office"
-  },
- {
-    data: "extension",
-    title: "Extn."
-  },
- {
-    data: "startDate",
-    title: "Start date"
-  },
- {
-    data: "salary",
-    title: "Salary"
-  },
- {
-    data: null,
-    title: "Actions",
-    name: "Actions",
-    render: function (data, type, row, meta) {
-      return '<a class="delbutton fa fa-minus-square btn btn-danger" href="#"></a>';
+$(document).ready(function () {
+  const columns = [
+    {
+      data: "id",
+      title: "Id",
+      type: "readonly",
     },
-    disabled: true
-  } 
+    {
+      data: "RagSoc",
+      title: "Ragione Sociale",
+    },
+    {
+      data: "Address",
+      title: "Indirizzo",
+    },
+    {
+      data: "City",
+      title: "Citta",
+    },
+    {
+      data: "Prov",
+      title: "Provincia",
+    },
+    {
+      data: "CAP",
+      title: "CAP date",
+    },
+    {
+      data: "PIVA",
+      title: "PIVA",
+    },
+    {
+      data: "CF",
+      title: "CF",
+    },
+    {
+      data: "Phone",
+      title: "Telefono",
+    },
+    {
+      data: "Fax",
+      title: "Fax",
+    },
+    {
+      data: "Email",
+      title: "Email",
+    },
+    {
+      data: null,
+      title: "Actions",
+      name: "Actions",
+      render: function (data, type, row, meta) {
+        return (
+          '<div class="container-flex"> <a class="editbutton myBtn btnBlue" href="#">Modify</a>' +
+          '<a class="delbutton myBtn btnRed" href="#">Delete</a> </div>'
+        );
+      },
+      disabled: true,
+    },
   ];
 
   var myTable;
 
-  myTable = $('#example').DataTable({
-    "sPaginationType": "full_numbers",
-    data: dataSet,
-    columns: columnDefs,
-	dom: 'Bfrtip',        // Needs button container
+  myTable = $("#example").DataTable({
+    sPaginationType: "full_numbers",
+    columns: columns,
+    dom: "Bfrtip",
     select: {
-        style: 'single',
-        toggleable: false
+      style: "single",
+      toggleable: false,
+    },
+    ajax: {
+      url: " http://localhost:3000/anagraficajson",
+      type: "GET",
+      dataSrc: "",
+      dataType: "json",
     },
     responsive: true,
-    altEditor: true,     // Enable altEditor
-    buttons: []          // no buttons, however this seems compulsory
+    altEditor: true,
+    buttons: [],
   });
 
   // Edit
-  $(document).on('click', "[id^='example'] tbody ", 'tr', function () {
-    var tableID = $(this).closest('table').attr('id');    // id of the table
-    var that = $( '#'+tableID )[0].altEditor;
+  $(document).on("click", "[id^='example'] tbody ", "tr", function () {
+    var tableID = $(this).closest("table").attr("id"); // id of the table
+    var that = $("#" + tableID)[0].altEditor;
     that._openEditModal();
-    $('#altEditor-edit-form-' + that.random_id)
-                .off('submit')
-                .on('submit', function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    that._editRowData();
-                });
+    $("#altEditor-edit-form-" + that.random_id)
+      .off("submit")
+      .on("submit", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that._editRowData();
+      });
   });
 
   // Delete
-  $(document).on('click', "[id^='example'] .delbutton", 'tr', function (x) {
-    var tableID = $(this).closest('table').attr('id');    // id of the table
-    var that = $( '#'+tableID )[0].altEditor;
+  $(document).on("click", "[id^='example'] .delbutton", "tr", function (x) {
+    var tableID = $(this).closest("table").attr("id"); // id of the table
+    var that = $("#" + tableID)[0].altEditor;
     that._openDeleteModal();
-    $('#altEditor-delete-form-' + that.random_id)
-                .off('submit')
-                .on('submit', function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    that._deleteRow();
-                });
+    $("#altEditor-delete-form-" + that.random_id)
+      .off("submit")
+      .on("submit", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that._deleteRow();
+      });
     x.stopPropagation(); //avoid open "Edit" dialog
   });
 
   // Add row
-  $('#addbutton').on('click', function () {
-    var that = $( '#example' )[0].altEditor;
+  $("#addbutton").on("click", function () {
+    var that = $("#example")[0].altEditor;
     that._openAddModal();
-    $('#altEditor-add-form-' + that.random_id)
-                .off('submit')
-                .on('submit', function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    that._addRowData();
-                });
+    $("#altEditor-add-form-" + that.random_id)
+      .off("submit")
+      .on("submit", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that._addRowData();
+      });
   });
 });
